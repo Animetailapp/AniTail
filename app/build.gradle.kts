605aa1eca858a363aa615df194bb016a54fb518e
Dark25 <nadiecaca2000@gmail.com>
2025-07-16 00:13:55 +0200

    chore(release): bump version code to 10 and version name to 1.9.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,8 +21,8 @@ android {
         applicationId = "com.anitail.music"
         minSdk = 21
         targetSdk = 36
-        versionCode = 9
-        versionName = "1.8.1"
+        versionCode = 10
+        versionName = "1.9.0"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables.useSupportLibrary = true
         
