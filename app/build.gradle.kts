752e12e6e4ec2ecbed653547b466aa92b0503993
Dark25 <nadiecaca2000@gmail.com>
2025-08-01 23:55:53 +0200

    feat(ui): implement standardized slider color configurations for player interface

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,8 +21,8 @@ android {
         applicationId = "com.anitail.music"
         minSdk = 21
         targetSdk = 36
-        versionCode = 11
-        versionName = "1.10.0"
+        versionCode = 12
+        versionName = "1.10.1"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables.useSupportLibrary = true
         
