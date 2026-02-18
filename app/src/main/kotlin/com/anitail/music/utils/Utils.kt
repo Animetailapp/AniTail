697249ef1358eb35f8a2650ea4bac619713b23f0
Dark25 <nadiecaca2000@gmail.com>
2026-02-18 18:23:01 +0100

    feat: add cancellation cause check in exception reporting utility

--- app/src/main/kotlin/com/anitail/music/utils/Utils.kt
+++ app/src/main/kotlin/com/anitail/music/utils/Utils.kt
@@ -1,5 +1,17 @@
 package com.anitail.music.utils
 
 fun reportException(throwable: Throwable) {
+    if (throwable.hasCancellationCause()) return
     throwable.printStackTrace()
+}
+
+private fun Throwable.hasCancellationCause(): Boolean {
+    var current: Throwable? = this
+    while (current != null) {
+        if (current is java.util.concurrent.CancellationException) {
+            return true
+        }
+        current = current.cause
+    }
+    return false
 }
