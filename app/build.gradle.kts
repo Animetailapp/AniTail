8e1297bc083b8377332be225603bc4c86cbff5ca
Dark25 <nadiecaca2000@gmail.com>
2026-02-23 18:18:15 +0100

    fix: actualizar la versión de la aplicación a 1.15.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -20,7 +20,7 @@ android {
         minSdk = 23
         targetSdk = 36
         versionCode = 17
-        versionName = "1.14.3"
+        versionName = "1.15.0"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables.useSupportLibrary = true
         
